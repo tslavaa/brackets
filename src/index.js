ca127b19ffb18829279b03dd10b2fd549355e885
tslavaa <70520545+tslavaa@users.noreply.github.com>
2021-02-27 23:40:56 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  module.exports = function check(str, bracketsConfig) {
+    if (str.length % 2 !== 0)  return false;
+    let input = str.split('');
+    let brackets = {};
+    let stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];    
+  }  
+  
+  for (let j = 0; j < input.length; j++) {  
+    if (brackets[stack[stack.length - 1]] === input[j])  {
+      stack.pop();        
+    } else { 
+      stack.push(input[j]);
+    }       
+  }
+
+        return stack.length === 0;
+    }
 }
